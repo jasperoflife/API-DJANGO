c35ecc59de73e1f5256adf71bd085c3a13006222
Iyeyemi Akinrinlola <iyeyemigodswill@gmail.com>
2022-12-20 14:56:42 +0100

    first commit

---
+++ api/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
+# from django.http import JsonResponse
+
+# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls ={
+        'List': '/task-list',
+        'Detail View': '/task-detail/<str:pk>',
+        'Create': '/task-create',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    #serializers represent your forms in api create context
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST']) #detail is same for update, just change the needful
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(data=request.data, instance=task)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE']) 
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    
+    return Response('item successfully deleted')
